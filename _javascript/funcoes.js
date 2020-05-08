dd917e4741c86a49fc3809a3286b7864ae8e7f17
Rafael Gábrio <rafael-sims@live.com>
2020-05-08 17:48:24 -0300

    Ajustes e novas funcionalidades no iframe de escolha

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,27 @@
 function escolha() {
     
 }
+
+function selecao(escolha) {
+    let img = document.getElementById('imgrepre');
+    switch (escolha) {
+        case 'quad':
+            img.src = '../_imagens/quad.jpg';
+            break;
+        case 'triang':
+            img.src = '../_imagens/triang.jpg';
+            break;
+        case 'circ':
+            img.src = '../_imagens/circ.png';
+            break;
+        case 'retang':
+            img.src = '../_imagens/retang.jpg';
+            break;
+        case 'paral':
+            img.src = '../_imagens/paral.png';
+            break;
+        case 'trap':
+            img.src = '../_imagens/trap.png';
+            break;    
+    }
+}
