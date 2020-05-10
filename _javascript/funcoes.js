8951f58a59dd3895fee2d9f5086c187aeff30454
Rafael Gábrio <rafael-sims@live.com>
2020-05-10 10:14:53 -0300

    Criação das funcionalidades do

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,5 +1,29 @@
+let cont = 0;
+let quad = document.getElementById('escQuad');
+let triang = document.getElementById('escTriang');
+let circ = document.getElementById('escCirc');
+let retang = document.getElementById('escRetang');
+let paral = document.getElementById('escParal');
+let trap = document.getElementById('escTrap');
+
 function escolha() {
-    
+    if (cont == 0){
+        if(quad.click) {
+            montaCalc(1);
+        } else if (triang.click) {
+            montaCalc(2);
+        } else if (circ.click) {
+            montaCalc(3);
+        } else if (retang.click) {
+            montaCalc(4);
+        } else if (paral.click) {
+            montaCalc(5);
+        } else if (trap.click) {
+            montaCalc(6);
+        }
+    } else if(true) {
+
+    }
 }
 
 function selecao(escolha) {
@@ -23,5 +47,9 @@ function selecao(escolha) {
         case 'trap':
             img.src = '../_imagens/trap.png';
             break;    
+    }
+
+    function montaCalc(opcao) {
+        
     }
 }
